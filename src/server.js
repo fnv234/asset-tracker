febe1ab012d658a67a4f2385c7b4a31902218a1b
Francesca Venditti <135988334+fnv234@users.noreply.github.com>
2024-08-02 09:15:25 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const path = require('path');
-const UploadPage = require('./UploadPage');
-const AnalysisPage = require('./AnalysisPage');
+// const UploadPage = require('./UploadPage');
+// const AnalysisPage = require('./AnalysisPage');
 
 
 const app = express();
 const port = 3000;
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes
-app.use('/upload', UploadPage);
-app.use('/analysis', AnalysisPage);
+// // Define routes
+// app.use('/upload', UploadPage);
+// app.use('/analysis', AnalysisPage);
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
